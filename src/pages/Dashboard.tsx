601dadfa478eb452c430e06376bdb42d53a7cc8f
friday adams <adamzfriday@gmail.com>
2026-07-22 12:27:31 +0100

    feat: implement protected application layout

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,15 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Dashboard Page
+      </h1>
+
+      <p className="mt-4">
+        If you can see this, ProtectedLayout is working!
+      </p>
+    </div>
+  );
+};
+
+export default Dashboard;
